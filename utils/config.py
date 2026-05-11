3045958d020d5418130b21be038f359755ad65ed
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:16:22 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,14 @@
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+MONGO_URI = getenv("MONGO_URI")
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+ENCRYPTION_KEY = getenv("ENCRYPTION_KEY")
