581fb29c01cd3b41388ac8b186a4f56fdc30d169
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:19:28 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,1 +1,34 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
 
+from database.users import create_user
+from bot.keyboards.main_keyboard import main_keyboard
+
+
+@Client.on_message(filters.command("start"))
+async def start_command(
+    client: Client,
+    message: Message
+):
+
+    user_id = message.from_user.id
+
+    await create_user(user_id)
+
+    text = f"""
+🚀 Welcome to Telegram Deployer
+
+Deploy your bots directly to:
+
+• Koyeb
+• Render
+• Heroku
+
+without opening hosting dashboards.
+"""
+
+    await message.reply_text(
+        text,
+        reply_markup=main_keyboard()
+    )
