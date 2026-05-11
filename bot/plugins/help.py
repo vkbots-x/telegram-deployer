dc457dc328dd26512c9e09944d14512efac1edc0
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:19:52 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -1,1 +1,24 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
 
+
+@Client.on_message(filters.command("help"))
+async def help_command(
+    client: Client,
+    message: Message
+):
+
+    text = """
+📚 Available Commands
+
+/start - Start bot
+/help - Show help
+
+/setgithub - Set GitHub token
+/setkoyeb - Set Koyeb token
+
+More deployment features coming soon.
+"""
+
+    await message.reply_text(text)
