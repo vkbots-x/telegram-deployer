c2e5b6cdfccbfd23ca0bb880c0a15adfe065847e
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:20:49 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,17 @@
+from pyrogram import Client
 
+from utils.config import (
+    API_ID,
+    API_HASH,
+    BOT_TOKEN
+)
+
+app = Client(
+    "telegram-deployer",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(
+        root="bot.plugins"
+    )
+)
