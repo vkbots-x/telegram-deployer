b9044f246e8c58fa8cf3f2ca26438c72dd25a420
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:16:50 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,29 @@
+import logging
+import colorlog
 
+
+def setup_logger():
+    handler = colorlog.StreamHandler()
+
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+        )
+    )
+
+    logger = colorlog.getLogger()
+
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+logger = setup_logger()
