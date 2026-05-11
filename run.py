3ade14b632ae03c49da11d813f9e019d8a888d75
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:21:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
+import uvloop
+import asyncio
 
+from bot.bot import app
+from utils.logger import logger
+
+uvloop.install()
+
+
+async def main():
+
+    logger.info(
+        "Starting Telegram Deployer Bot..."
+    )
+
+    await app.start()
+
+    me = await app.get_me()
+
+    logger.info(
+        f"Bot started as @{me.username}"
+    )
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
