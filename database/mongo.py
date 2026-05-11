aaa02901bbc63921d105424bdfe8b0e897d624fa
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:17:51 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,1 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from utils.config import MONGO_URI
+from utils.logger import logger
 
+mongo_client = AsyncIOMotorClient(MONGO_URI)
+
+db = mongo_client["telegram_deployer"]
+
+logger.info("MongoDB connected successfully")
