e006da6a1e76252a061c2261235dc8233ae74739
vkbots-x <vkbots666@gmail.com>
2026-05-11 21:17:15 +0530

    Update encryption.py

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,1 +1,12 @@
+from cryptography.fernet import Fernet
+from utils.config import ENCRYPTION_KEY
 
+cipher = Fernet(ENCRYPTION_KEY.encode())
+
+
+def encrypt_text(text: str) -> str:
+    return cipher.encrypt(text.encode()).decode()
+
+
+def decrypt_text(text: str) -> str:
+    return cipher.decrypt(text.encode()).decode()
