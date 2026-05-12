4f25781de0a8363d35287e184a24576c10f1756f
vkbots-x <vkbots666@gmail.com>
2026-05-12 09:07:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-import asyncio
 import threading
 
 import uvicorn
+
+from pyrogram import idle
 
 from bot.bot import app
 from utils.logger import logger
@@ -10,40 +11,40 @@ from web import app as web_app
 
 
 def run_web():
+
     uvicorn.run(
         web_app,
         host="0.0.0.0",
-        port=10000
+        port=10000,
+        log_level="info"
     )
 
 
-async def run_bot():
+def main():
 
     logger.info(
         "Starting Telegram Deployer Bot..."
     )
 
-    await app.start()
+    thread = threading.Thread(
+        target=run_web
+    )
 
-    me = await app.get_me()
+    thread.daemon = True
+    thread.start()
+
+    app.start()
+
+    me = app.get_me()
 
     logger.info(
         f"Bot started as @{me.username}"
     )
 
-    await asyncio.Event().wait()
+    idle()
 
-
-async def main():
-
-    thread = threading.Thread(
-        target=run_web
-    )
-
-    thread.start()
-
-    await run_bot()
+    app.stop()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
