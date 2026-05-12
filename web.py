b5af470c330a6cffa69b7f13835740820eae421e
vkbots-x <vkbots666@gmail.com>
2026-05-12 10:13:25 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from fastapi import Request
 
-app = FastAPI()
+from bot.bot import app
+
+web_app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@web_app.get("/")
+async def home():
+
     return {
         "status": "running"
+    }
+
+
+@web_app.post("/webhook")
+async def webhook_handler(
+    request: Request
+):
+
+    update = await request.json()
+
+    await app.process_update(update)
+
+    return {
+        "ok": True
     }
